ee4127d4e935ff6aa883ff6a622d82629c3f6f9c
FreememGetmem <morndour83@hotmail.com>
2026-01-27 18:13:38 -0800

    Wistia pipeline project 31

--- pyspark/wistia.etl.py
+++ pyspark/wistia.etl.py
@@ -23,6 +23,7 @@ RAW_BASE = "s3://wistia-data/data_raw/"
 DWH_BASE = "s3://wistia-data/data_curated/"
 
 MEDIA_PATH = f"{RAW_BASE}/media/*/*.json"
+
 EVENTS_PATH = f"{RAW_BASE}/events/*/*/*.json"
 VISITORS_PATH = f"{RAW_BASE}/visitors/*/*/*.json"
 
