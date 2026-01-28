0e630ad8d99a5d8a31be4e5f61d5260e1a2790c2
FreememGetmem <morndour83@hotmail.com>
2026-01-27 20:16:25 -0800

    wistia-after 76

--- pyspark/transform_wistia.py
+++ pyspark/transform_wistia.py
@@ -15,8 +15,8 @@ logger = logging.getLogger(__name__)
 logger.info("Starting Spark session")
 spark = SparkSession.builder.appName("wistia-transform").getOrCreate()
 
-RAW = "s3://wistia-data/data_raw/"
-DWH = "s3://wistia-data/data_curated/"
+RAW = "s3://wistia-pipeline-635374934580-us-west-1/data_raw/"
+DWH = "s3://wistia-pipeline-635374934580-us-west-1/data_curated/"
 
 # ----------------------------------------------------------
 # Load Raw Data
